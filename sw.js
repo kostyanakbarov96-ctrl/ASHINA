b43f421be05b4de2ddfc8d88f4857ac6372283cb
kostyanakbarov96-ctrl <kostyan.akbarov.96@mail.ru>
2026-08-31 11:28:39 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,90 @@
-const C="ashina-test-1-1";const A=["./","./index.html","./manifest.webmanifest","./audio.wav"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE_NAME = "ashina-cache-1-7";
+
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./audio.wav"
+];
+
+/* Установка нового Service Worker */
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+
+      .then(cache => cache.addAll(APP_FILES))
+
+      .then(() => self.skipWaiting())
+
+  );
+
+});
+
+
+/* Активация нового Service Worker */
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+
+    caches.keys()
+
+      .then(keys =>
+
+        Promise.all(
+
+          keys
+
+            .filter(key => key !== CACHE_NAME)
+
+            .map(key => caches.delete(key))
+
+        )
+
+      )
+
+      .then(() => self.clients.claim())
+
+  );
+
+});
+
+
+/* Обработка запросов */
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+
+      .then(cachedResponse => {
+
+        if (cachedResponse) {
+
+          return cachedResponse;
+
+        }
+
+        return fetch(event.request)
+
+          .then(networkResponse => {
+
+            return networkResponse;
+
+          })
+
+          .catch(() => {
+
+            return caches.match("./index.html");
+
+          });
+
+      })
+
+  );
+
+});
